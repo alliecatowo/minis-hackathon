9e28db701757a43f517df410d31e20a97f8f9e5a
Allie <alliecatowo@users.noreply.github.com>
2026-04-25 10:15:40 -0700

    temp validate post-merge fixes

--- backend/alembic/versions/20260425120000_add_ai_contamination_verdict_provenance_MINI_208.py
+++ backend/alembic/versions/20260425120000_add_ai_contamination_verdict_provenance_MINI_208.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "20260425120000"
-down_revision: Union[str, tuple[str, str], None] = ("20260424153000", "20260424183000")
+down_revision: Union[str, None] = "20260425100000"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
